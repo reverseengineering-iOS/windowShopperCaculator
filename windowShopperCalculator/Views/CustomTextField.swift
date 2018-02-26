501bdc003c8423f88fc8a1720a1e37bb5c737ee3
sai <saivishal.ios@gmail.com>
2018-02-26 09:07:12 -0500

    1)created a custom UITextFiel

---
+++ windowShopperCalculator/Views/CustomTextField.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTextField.swift
+//  windowShopperCalculator
+//
+//  Created by Saivishal Gummadi on 2/26/18.
+//  Copyright © 2018 ReverseEngineering. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2492508562)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor:#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+    }
+
+}
